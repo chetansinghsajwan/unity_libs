887f59a45eba472ed2e88c091bb3fdc894584fd4
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2022-09-14 12:59:30 +0530

    Added TransformExtensions

---
+++ Runtime/Extensions/TransformExtensions.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace GameFramework.Extensions
+{
+    public static class TransformExtensions
+    {
+        public static void SetWorldScale(this Transform transform, Vector3 globalScale)
+        {
+            Vector3 worldScale = transform.lossyScale;
+            transform.localScale = new Vector3(globalScale.x / worldScale.x,
+                globalScale.y / worldScale.y, globalScale.z / worldScale.z);
+        }
+    }
+}
