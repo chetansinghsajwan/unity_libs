bb70dbb643f13647c465fcb7eceaba15f1432d8b
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2022-09-14 11:47:23 +0530

    Added Static API for LogManagerSystem

--- Runtime/LogManagement/LogManager.cs
+++ Runtime/LogManagement/LogManager.cs
@@ -11,5 +11,30 @@ namespace GameFramework.LogManagement
         public static ILogger Logger = new SilentLogger();
 
         public static LogLevel DefaultLogLevel = LogLevel.Information;
+
+        public static ILogger CreateLogger(string category, params ILogTarget[] logTargets)
+        {
+            return CreateLogger(category, logTargets as IEnumerable<ILogTarget>);
+        }
+
+        public static ILogger CreateLogger(string category, IEnumerable<ILogTarget> logTargets)
+        {
+            return System.CreateLogger(category, logTargets);
+        }
+
+        public static ILogger CreateSubLogger(ILogger logger, string category, params ILogTarget[] logTargets)
+        {
+            return CreateSubLogger(logger, category, logTargets as IEnumerable<ILogTarget>);
+        }
+
+        public static ILogger CreateSubLogger(ILogger logger, string category, IEnumerable<ILogTarget> logTargets)
+        {
+            return System.CreateSubLogger(logger, category, logTargets);
+        }
+
+        public static string GetAddressFor(string logFile)
+        {
+            return System.GetAddressFor(logFile);
+        }
     }
 }
