a251b272471a695a4fd78508d65380c798fe61fd
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2022-09-18 16:40:16 +0530

    Improved SceneAssetPropertyDrawer, refactored code and added indicator to show UnitySceneAsset status

--- Editor/SceneManagement/SceneAssetPropertyDrawer.cs
+++ Editor/SceneManagement/SceneAssetPropertyDrawer.cs
@@ -1,5 +1,4 @@
 using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 using UnitySceneAsset = UnityEditor.SceneAsset;
@@ -13,6 +12,7 @@ namespace GameFramework
         protected const float YBoxPadSize = 4f;
         protected const float XSeparatorSize = 2f;
         protected const float YSeparatorSize = 2f;
+        protected const float FieldValueFactor = 0.612f;
 
         protected static readonly float LineHeight = EditorGUIUtility.singleLineHeight + 1f;
         protected static readonly GUIContent BoxGUIContent = GUIContent.none;
@@ -27,58 +27,60 @@ namespace GameFramework
             position.width -= XBoxPadSize;
             Rect startPosition = position;
 
-            EditorGUI.BeginProperty(position, label, property);
-
-            // serialized object and properties
-            SerializedProperty propSceneAsset = property.FindPropertyRelative("_sceneAsset");
-
-            // UnitySceneAsset value stored in SceneReference for holding direct reference
-            UnitySceneAsset sceneAsset = propSceneAsset.objectReferenceValue as UnitySceneAsset;
-
-            // create background box
-            Rect boxRect = new Rect
+            using (new EditorGUI.PropertyScope(position, label, property))
             {
-                x = position.x - XBoxPadSize,
-                y = position.y - YBoxPadSize,
-                width = position.width + XBoxPadSize + XBoxPadSize, // 2 pads for left and right
+                // serialized object and properties
+                SerializedProperty propSceneAsset = property.FindPropertyRelative("_sceneAsset");
 
-                height = finalHeight + YBoxPadSize + YBoxPadSize    // // 2 pads for up and down
-                // [height] is based on [_height] which is calculated later,
-                // this creates an lagging effect in th UI, 
-                // where property background box is expanded one frame after clicking
-                // 
-                // this approach is necessary to keep a lose connection between
-                // expanded property options and background box
-            };
+                // UnitySceneAsset value stored in SceneReference for holding direct reference
+                UnitySceneAsset sceneAsset = propSceneAsset.objectReferenceValue as UnitySceneAsset;
 
-            GUI.Box(boxRect, BoxGUIContent, BoxGUIStyle);
-
-            // scene asset field
-            using (var scope = new EditorGUI.ChangeCheckScope())
-            {
-                // editor field for assigning UnitySceneAsset
-                sceneAsset = EditorGUI.ObjectField(
-                    // -30f to leave space for advanced options foldout
-                    new Rect(position.x, position.y, position.width - 30f, position.height),
-                    label, sceneAsset, typeof(UnitySceneAsset), false) as UnitySceneAsset;
-
-                if (scope.changed)
+                // create background box
+                Rect boxRect = new Rect
                 {
-                    propSceneAsset.objectReferenceValue = sceneAsset;
+                    x = position.x - XBoxPadSize,
+                    y = position.y - YBoxPadSize,
+                    width = position.width + XBoxPadSize + XBoxPadSize, // 2 pads for left and right
+
+                    height = finalHeight + YBoxPadSize + YBoxPadSize    // 2 pads for up and down
+                    // [height] is based on [_height] which is calculated later,
+                    // this creates an lagging effect in th UI, 
+                    // where property background box is expanded one frame after clicking
+                    // 
+                    // this approach is necessary to keep a lose connection between
+                    // expanded property options and background box
+                };
+
+                GUI.Box(boxRect, BoxGUIContent, BoxGUIStyle);
+
+                // scene asset field
+                using (var scope = new EditorGUI.ChangeCheckScope())
+                {
+                    // editor field for assigning UnitySceneAsset
+                    sceneAsset = EditorGUI.ObjectField(
+                        // -30f to leave space for advanced options foldout
+                        new Rect(position.x, position.y, position.width - 30f, position.height),
+                        label, sceneAsset, typeof(UnitySceneAsset), false) as UnitySceneAsset;
+
+                    if (scope.changed)
+                    {
+                        propSceneAsset.objectReferenceValue = sceneAsset;
+                    }
+                }
+
+                // shows a indicator to represent state of scene asset
+                ShowIndicator(ref position, property, sceneAsset);
+
+                // advanced options foldout
+                property.isExpanded = EditorGUI.Foldout(
+                    new Rect(position.width + 10f, position.y, 20f, position.height),
+                    property.isExpanded, GUIContent.none);
+
+                if (property.isExpanded)
+                {
+                    ShowInfo(ref position, property, label);
                 }
             }
-
-            // advanced options foldout
-            property.isExpanded = EditorGUI.Foldout(
-                new Rect(position.width + 10f, position.y, 20f, position.height),
-                property.isExpanded, GUIContent.none);
-
-            if (property.isExpanded)
-            {
-                ShowInfo(ref position, property, label);
-            }
-
-            EditorGUI.EndProperty();
 
             finalHeight = position.y - startPosition.y + position.height;
         }
@@ -109,8 +111,10 @@ namespace GameFramework
                 isEnabledInBuild = editorScene.enabled;
             }
 
-            position.width = position.width * 0.612f;
-            position.x = position.x + 160f;
+            // we display buttons only to field value side,
+            // so set width accordingly
+            position.x += position.width * (1f - FieldValueFactor);
+            position.width = position.width * FieldValueFactor;
 
             // button to add scene to build settings
             // only available when scene is not in build settings
@@ -132,7 +136,6 @@ namespace GameFramework
             // button to enable scene in build settings
             // only available when scene is in build settings,
             // but not enabled
-            // using (new EditorGUI.DisabledGroupScope(isInBuild is false && isEnabledInBuild))
             using (new EditorGUI.DisabledGroupScope(sceneAsset is null || isInBuild is false || isEnabledInBuild))
             {
                 position.y += position.height + YSeparatorSize;
@@ -166,7 +169,52 @@ namespace GameFramework
             }
         }
 
-        protected EditorBuildSettingsScene GetEditorScene(UnitySceneAsset sceneAsset)
+        protected virtual void ShowIndicator(ref Rect position, SerializedProperty property, UnitySceneAsset sceneAsset)
+        {
+            // referenced from: https://github.com/r2d2meuleu/unity-scene-reference.git
+
+            // values to provide scene status
+            bool isInBuild = false;
+            bool isEnabledInBuild = true;
+
+            var editorScene = GetEditorScene(sceneAsset);
+            if (editorScene is not null)
+            {
+                isInBuild = true;
+                isEnabledInBuild = editorScene.enabled;
+            }
+
+            GUIContent content = null;
+
+            // not in build scenes
+            if (isInBuild is false)
+            {
+                content = EditorGUIUtility.IconContent("d_winbtn_mac_close");
+                content.tooltip = "This scene is NOT in build settings.\nIt will be NOT included in builds.";
+            }
+            // in build scenes but disabled
+            else if (isEnabledInBuild is false)
+            {
+                content = EditorGUIUtility.IconContent("d_winbtn_mac_min");
+                content.tooltip = @"This scene is in build settings but DISABLED.
+                                        It will be NOT included in builds.";
+            }
+            // in build scenes and enabled
+            else
+            {
+                content = EditorGUIUtility.IconContent("d_winbtn_mac_max");
+                content.tooltip = @"This scene is in build settings and ENABLED.
+                                        It will be included in builds.";
+            }
+
+            int controlId = GUIUtility.GetControlID(FocusType.Passive);
+            Rect indicatorPosition = position;
+            indicatorPosition.x = position.width - 5f;
+
+            EditorGUI.PrefixLabel(indicatorPosition, controlId, content);
+        }
+
+        protected static EditorBuildSettingsScene GetEditorScene(UnitySceneAsset sceneAsset)
         {
             if (sceneAsset is not null)
             {
@@ -185,16 +233,20 @@ namespace GameFramework
             return null;
         }
 
-        protected void AddSceneToBuildSettings(UnitySceneAsset sceneAsset, bool enabled = true)
+        protected static void AddSceneToBuildSettings(UnitySceneAsset sceneAsset, bool enabled = true)
         {
-            GUID guid = AssetDatabase.GUIDFromAssetPath(AssetDatabase.GetAssetPath(sceneAsset));
+            if (sceneAsset is not null)
+            {
+                GUID guid = AssetDatabase.GUIDFromAssetPath(
+                    AssetDatabase.GetAssetPath(sceneAsset));
 
-            EditorBuildSettingsScene[] editorScenes = EditorBuildSettings.scenes;
-            ArrayUtility.Add(ref editorScenes, new EditorBuildSettingsScene(guid, enabled));
-            EditorBuildSettings.scenes = editorScenes;
+                EditorBuildSettingsScene[] editorScenes = EditorBuildSettings.scenes;
+                ArrayUtility.Add(ref editorScenes, new EditorBuildSettingsScene(guid, enabled));
+                EditorBuildSettings.scenes = editorScenes;
+            }
         }
 
-        protected bool EnableSceneInBuildSettings(UnitySceneAsset sceneAsset, bool enable = true)
+        protected static bool EnableSceneInBuildSettings(UnitySceneAsset sceneAsset, bool enable = true)
         {
             if (sceneAsset is not null)
             {
@@ -219,6 +271,8 @@ namespace GameFramework
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
+            // final height is updated in OnGUI()
+            // to keep editor dynamically and easily extensible
             return finalHeight;
         }
     }
